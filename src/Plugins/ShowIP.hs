3471d33c8f88f08d085f60b8452604affba8d26c
Christos Tsiliakis <mail@tsiliakis.net>
2013-06-29 15:47:14 +0200

    made ShowIP more generic

--- src/Plugins/ShowIP.hs
+++ src/Plugins/ShowIP.hs
@@ -9,6 +9,9 @@
 -- Portability :  untested
 --
 -- A simple plugin to display the external IP or any other String from a webserver.
+-- As a simple server that returns a IP, feel free to use my server at 
+-- http://srv.u0.org/ip.php , but I can't guarantee that it will be up forever.
+-- Other choices are http://wtfismyip.com/text or just google ;) 
 --
 -----------------------------------------------------------------------------
 
@@ -18,13 +21,13 @@ import Plugins
 import Network.HTTP
 
 
-data ShowIP = ShowIP String Int deriving (Read, Show)
+data ShowIP = ShowIP String String Int deriving (Read, Show)
 
 -- | Retrieves the string from the given address.
 -- Note, that the webserver has to reply with a simple string.
-getIP :: IO String
-getIP = do ip <- simpleHTTP (getRequest "http://srv.u0.org/ip.php") >>=  fmap (take 100) . getResponseBody
-           return $ stripWspc ip
+getIP :: String -> IO String
+getIP url = do ip <- simpleHTTP (getRequest url ) >>=  fmap (take 100) . getResponseBody
+               return $ stripWspc ip
                       
 -- | Clears a string from spaces, new lines or tabs
 stripWspc :: String -> String
@@ -32,11 +35,11 @@ stripWspc [] = []
 stripWspc list = filter (\x -> notElem x " \n\t") list
                                                                    
 -- | Returns the string with a prefix string.
-printIP :: String -> IO String                       
-printIP preFix = do ip <- getIP
-                    return $ preFix ++ ip
+printIP :: String -> String -> IO String                       
+printIP preFix url = do ip <- getIP url
+                        return $ preFix ++ ip
                         
 instance Exec ShowIP where
-    alias ( ShowIP _ _ ) = "ShowIP"
-    run   ( ShowIP s _ ) = printIP s
-    rate  ( ShowIP _ r ) = r
+    alias ( ShowIP _ _ _ ) = "ShowIP"
+    run   ( ShowIP s u _ ) = printIP s u
+    rate  ( ShowIP _ _ r ) = r
